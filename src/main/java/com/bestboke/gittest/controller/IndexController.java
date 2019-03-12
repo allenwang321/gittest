25843af53d211d81a282f7b72fd6ed4ce7ea554f
allenwang321 <zbxhui@gmail.com>
2019-03-12 15:46:06 +0800

    修改代码

--- src/main/java/com/bestboke/gittest/controller/IndexController.java
+++ src/main/java/com/bestboke/gittest/controller/IndexController.java
@@ -2,11 +2,13 @@ package com.bestboke.gittest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping(value = {"/", "index.html"})
+    @ResponseBody
     public String index(){
         return "ok";
     }
