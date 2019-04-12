b8c74738844c0a90286f49fba589d6a79bcb03f9
allenwang321 <zbxhui@gmail.com>
2019-04-12 11:35:29 +0800

    新增测试post

--- src/main/java/com/bestboke/gittest/controller/IndexController.java
+++ src/main/java/com/bestboke/gittest/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.bestboke.gittest.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +14,12 @@ public class IndexController {
     @ResponseBody
     public String index(){
         return "测试自动部署 啊哈哈哈或";
+    }
+
+    @PostMapping("json")
+    @ResponseBody
+    public String jsonTest(@RequestBody JSONObject jsonObject){
+        return jsonObject.toJSONString();
     }
 
 }
